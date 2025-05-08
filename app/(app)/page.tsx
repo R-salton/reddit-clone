2174577270521df18f33cf622414480b8be9553d
R-salton <ringusa22@gmail.com>
2025-05-08 15:48:30 +0200

    Added Clerk Auth

---
+++ app/(app)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="">
+     <h1>Hello Sadev</h1>
+     <Button>Click me</Button>
+
+
+    </div>
+  );
+}
